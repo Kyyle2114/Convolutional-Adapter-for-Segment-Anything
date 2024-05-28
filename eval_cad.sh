fdd23d6a615d1c2e9128f6e9d784831ea9d0b5f3
Kyyle <kyyle2114@gmail.com>
2024-05-29 00:43:46 +0900

    Fix: .sh files

--- eval_cad.sh
+++ eval_cad.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 eval.py --batch_size 4 --seed 21 --model_type vit_b --checkpoint sam_vit_b.pth 
+python3 eval_cad.py --batch_size 4 --seed 21 --model_type vit_b --checkpoint sam_vit_b.pth 
--- run_cad.sh
+++ run_cad.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 run.py --batch_size 4 --port 1234 --dist False --seed 21 --model_type vit_b --checkpoint sam_vit_b.pth --epoch 20 --lr 2e-4 --project_name SAM-CAD
+python3 run_cad.py --batch_size 4 --port 1234 --dist False --seed 21 --model_type vit_b --checkpoint sam_vit_b.pth --epoch 20 --lr 2e-4 --project_name SAM-CAD
